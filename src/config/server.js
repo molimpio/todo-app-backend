6fa893a3713f2a735e5b482956c52ad97bf67f82
Moises <moises@rcbdesenvolvimento.com.br>
2018-08-23 19:45:05 -0300

    configuracao api com mongoose

--- src/config/server.js
+++ src/config/server.js
@@ -7,3 +7,5 @@ server.use(bodyParser.urlencoded({ extended: true}))
 server.use(bodyParser.json())
 
 server.listen(port, () => console.log(`Backend running port ${port}`))
+
+module.exports = server
